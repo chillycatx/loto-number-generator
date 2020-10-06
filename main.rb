a38d498b262b6d06d29b37159f62b62878532a6a
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-10-06 14:01:25 +0000

    Applied code standarization on existing code.

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 seed = ARGV.length == 1 && ARGV[0].to_i != 0 ? ARGV[0].to_i : Random.new_seed
 random = Random.new(seed)
@@ -7,32 +8,29 @@ step = 15
 current_letter = 1
 
 hash = {
-    'B': {},
-    'I': {},
-    'N': {},
-    'G': {},
-    'O': {}
+  'B': {},
+  'I': {},
+  'N': {},
+  'G': {},
+  'O': {}
 }
 
 hash.each do |letter, numbers|
-    used = []
+  used = []
 
-    while numbers.length < 5
-        got_number = false
-        
-        while not got_number
-            random_number = random.rand(1 + step * current_letter - step...step * current_letter)
+  while numbers.length < 5
+    got_number = false
 
-            unless used.include? random_number
-                got_number = true
-            end
-        end
-
-        used << random_number
-        hash[letter][hash[letter].length] = random_number
+    until got_number
+      random_number = random.rand(1 + step * current_letter - step...step * current_letter)
+      got_number = true unless used.include? random_number
     end
 
-    current_letter += 1
+    used << random_number
+    hash[letter][hash[letter].length] = random_number
+  end
+
+  current_letter += 1
 end
 
 template = '+----+----+----+----+----+
@@ -53,11 +51,10 @@ template = '+----+----+----+----+----+
 numbers = []
 
 (0...5).each do |index|
-    hash.values.each do |balls|
-        numbers << balls[index]
-    end
+  hash.values.each do |balls|
+    numbers << balls[index]
+  end
 end
 
 puts "Generated from seed: #{seed}\n\n"
 puts template % numbers
-
