065faf9b6e7c4c349f55b68a8f3f33392511abc3
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2021-10-04 00:14:14 +0000

    Fix for generator, now generates highest numbers like (15, 30, 45, 60, 75).

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# frozen_string_literal: true
 
 seed = ARGV.length == 1 && ARGV[0].to_i != 0 ? ARGV[0].to_i : Random.new_seed
 random = Random.new(seed)
@@ -8,29 +7,35 @@ step = 15
 current_letter = 1
 
 hash = {
-  'B': {},
-  'I': {},
-  'N': {},
-  'G': {},
-  'O': {}
+    'B': {},
+    'I': {},
+    'N': {},
+    'G': {},
+    'O': {}
 }
 
 hash.each do |letter, numbers|
-  used = []
+    used = []
 
-  while numbers.length < 5
-    got_number = false
+    while numbers.length < 5
+        got_number = false
+        
+        while not got_number
+            low_step = 1 + step * current_letter - step
+            high_step = step * current_letter + 1
 
-    until got_number
-      random_number = random.rand(1 + step * current_letter - step...step * current_letter)
-      got_number = true unless used.include? random_number
+            random_number = random.rand(low_step...high_step)
+
+            unless used.include? random_number
+                got_number = true
+            end
+        end
+
+        used << random_number
+        hash[letter][hash[letter].length] = random_number
     end
 
-    used << random_number
-    hash[letter][hash[letter].length] = random_number
-  end
-
-  current_letter += 1
+    current_letter += 1
 end
 
 template = '+----+----+----+----+----+
@@ -51,10 +56,9 @@ template = '+----+----+----+----+----+
 numbers = []
 
 (0...5).each do |index|
-  hash.values.each do |balls|
-    numbers << balls[index]
-  end
+    hash.values.each do |balls|
+        numbers << balls[index]
+    end
 end
 
-puts "Generated from seed: #{seed}\n\n"
 puts template % numbers
